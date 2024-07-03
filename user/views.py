fbe08866127dfabd35cc79650e735d9058b5149a
Mannopboy <mannopboy8@gmail.com>
2024-07-03 17:07:50 +0500

    login

--- user/views.py
+++ user/views.py
@@ -5,19 +5,6 @@ from rest_framework import generics
 from .serializers import *
 
 
-# def loginUser(request):
-#     if request.method == 'POST':
-#         username = request.POST['username'].lower()
-#         password = request.POST['password']
-#
-#         user = authenticate(request, username=username, password=password)
-#         print(user)
-#         if user is not None:
-#             return True
-#         else:
-#             return False
-#     return render(request, '')
-
 
 
 class UserListCreateView(generics.ListCreateAPIView):
