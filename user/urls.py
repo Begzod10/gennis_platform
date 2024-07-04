43ff1687c9797328eb4cd69ca28870671be871d6
Mannopboy <mannopboy8@gmail.com>
2024-07-04 14:43:16 +0500

    scsdc

--- user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('users/', UserListCreateView.as_view(), name='user-list-create'),
-    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-]
