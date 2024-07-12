d75e99d7d3bca0f8b75916e72b40fd15078ceb26
shahzodbeeck <shahzodomonboyev1.com>
2024-07-12 15:18:50 +0500

    f

--- user/serializers.py
+++ user/serializers.py
@@ -1,36 +1,40 @@
 from django.contrib.auth.models import User
-
 from rest_framework import serializers
+
 from user.models import CustomUser, UserSalaryList, UserSalary, Branch
-from branch.serializers import BranchSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'name', 'surname', 'username', 'father_name', 'password', 'birth_date',
-                  'phone', 'age', 'profile_img', 'observer', 'comment', 'registered_date', 'branch', 'language']
-
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['id', 'name', 'surname', 'username', 'father_name', 'password',
+                  'phone', 'age', 'profile_img', 'observer', 'comment', 'registered_date', 'birth_date', 'language',
+                  'branch']
+        extra_kwargs = {
+            'password': {'write_only': True, 'required': True},
+            'birth_date': {'required': True},
+            'language': {'required': True},
+            'branch': {'required': True}
+        }
 
     def create(self, validated_data):
         user = CustomUser.objects.create(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            name=validated_data.get('name', ''),
-            surname=validated_data.get('surname', ''),
-            father_name=validated_data.get('father_name', ''),
+            name=validated_data.get('name'),
+            surname=validated_data.get('surname'),
+            username=validated_data.get('username'),
+            father_name=validated_data.get('father_name'),
+            password=validated_data.get('password'),
+            phone=validated_data.get('phone'),
+            age=validated_data.get('age'),
+            profile_img=validated_data.get('profile_img'),
+            observer=validated_data.get('observer'),
+            comment=validated_data.get('comment'),
+            registered_date=validated_data.get('registered_date'),
             birth_date=validated_data.get('birth_date'),
-            phone=validated_data.get('phone', ''),
-            age=validated_data.get('age', ''),
-            profile_img=validated_data.get('profile_img', ''),
-            observer=validated_data.get('observer', False),
-            comment=validated_data.get('comment', ''),
-            branch_id=validated_data.get('branch'),
-            language_id=validated_data.get('language'),
+            language=validated_data.get('language'),
+            branch=validated_data.get('branch')
         )
         return user
-
     def update(self, instance, validated_data):
         instance.username = validated_data.get('username', instance.username)
         if 'password' in validated_data:
@@ -46,6 +50,8 @@ class UserSerializer(serializers.ModelSerializer):
         instance.comment = validated_data.get('comment', instance.comment)
         instance.save()
         return instance
+
+
 class UserSalaryListSerializers(serializers.ModelSerializer):
     # user = UserSerializer(read_only=True)
     # branch = BranchSerializer(read_only=True    )
