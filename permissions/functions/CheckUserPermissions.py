1f41890d81bc86ca1fbf4a2670f853d70c04655a
thesobirov <shahzodsobirjonov9@gmail.com>
2024-07-20 14:32:44 +0500

    updated function check user permissions

--- permissions/functions/CheckUserPermissions.py
+++ permissions/functions/CheckUserPermissions.py
@@ -3,14 +3,9 @@ from django.contrib.auth.models import Permission
 
 def check_user_permissions(user, table_names):
     system = user.branch.location.system.id
-    base_permissions_data = Permission.objects.filter(content_type__model__in=table_names)
-    base_permissions = [permission.codename for permission in base_permissions_data]
+    base_permissions = Permission.objects.filter(content_type__model__in=table_names).values_list('codename', flat=True)
 
-    user_permissions = []
-    groups = user.groups.filter(permissions__content_type__model__in=table_names)
-    for group in groups:
-        if group.authgroupsystem.system_id.id == system:
-            user_permissions.extend(permission.codename for permission in group.permissions.all())
-
-    fl_pr = {pr: pr in user_permissions for pr in base_permissions}
-    return fl_pr
+    user_permissions = user.groups.filter(permissions__content_type__model__in=table_names,
+                                          authgroupsystem__system_id=system).values_list('permissions__codename',
+                                                                                         flat=True)
+    return {pr: pr in user_permissions for pr in base_permissions}
