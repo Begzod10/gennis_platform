12cf187951c605a5de7d0ebf80fae7e4077074e8
Mannopboy <mannopboy8@gmail.com>
2024-07-23 13:18:13 +0500

    changed subjects,system and tasks views

--- system/views.py
+++ system/views.py
@@ -1,26 +1,41 @@
-
-# Create your views here.
-
 from rest_framework import generics
-
 from .serializers import (
-    System, SystemSerializers
+    SystemSerializers
 )
-
-from django.shortcuts import render
+from .models import System
+from user.functions.functions import check_auth
+from rest_framework.response import Response
+from permissions.functions.CheckUserPermissions import check_user_permissions
 
 
 class CreateSystemList(generics.ListCreateAPIView):
     queryset = System.objects.all()
     serializer_class = SystemSerializers
-    # permission_classes = (
-    #     IsAuthenticatedOrReadOnly, IsAdminOrReadOnly)  # login qilgan yoki yuq ligini va admin emasligini tekshiradi
 
+    def get(self, request, *args, **kwargs):
+        user, auth_error = check_auth(request)
+        if auth_error:
+            return Response(auth_error)
+
+        table_names = ['system']
+        permissions = check_user_permissions(user, table_names)
+
+        queryset = System.objects.all()
+        serializer = SystemSerializers(queryset, many=True)
+        return Response({'systems': serializer.data, 'permissions': permissions})
 
 
 class SystemRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = System.objects.all()
     serializer_class = SystemSerializers
-    # permission_classes = (
-    #     IsAuthenticatedOrReadOnly, IsAdminOrReadOnly)  # login qilgan yoki yuq ligini va admin emasligini tekshiradi
 
+    def retrieve(self, request, *args, **kwargs):
+        user, auth_error = check_auth(request)
+        if auth_error:
+            return Response(auth_error)
+
+        table_names = ['system']
+        permissions = check_user_permissions(user, table_names)
+        system = self.get_object()
+        system_data = self.get_serializer(system).data
+        return Response({'system': system_data, 'permissions': permissions})
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,39 @@
-# views.py
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
+from user.functions.functions import check_auth
+from rest_framework.response import Response
+from permissions.functions.CheckUserPermissions import check_user_permissions
+
 
 class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
+    def get(self, request, *args, **kwargs):
+        user, auth_error = check_auth(request)
+        if auth_error:
+            return Response(auth_error)
+
+        table_names = ['task', 'auth_group', 'branch']
+        permissions = check_user_permissions(user, table_names)
+
+        queryset = Task.objects.all()
+        serializer = TaskSerializer(queryset, many=True)
+        return Response({'tasks': serializer.data, 'permissions': permissions})
+
+
 class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        user, auth_error = check_auth(request)
+        if auth_error:
+            return Response(auth_error)
+
+        table_names = ['task', 'auth_group', 'branch']
+        permissions = check_user_permissions(user, table_names)
+        task = self.get_object()
+        task_data = self.get_serializer(task).data
+        return Response({'task': task_data, 'permissions': permissions})
