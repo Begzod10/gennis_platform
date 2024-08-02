b929002f33093d7f40d524fb215bf45725eac137
thesobirov <shahzodsobirjonov9@gmail.com>
2024-08-02 21:03:26 +0500

    acs

--- attendances/Api/functions/CheckAndCalculateBlackSalary.py
+++ attendances/Api/functions/CheckAndCalculateBlackSalary.py
@@ -2,7 +2,7 @@ from students.models import Student
 
 
 def check_and_calculate_black_salary(student):
-    if student['debt_status'] > 0:
+    if student['debt_status'] == 2:
         return True
     else:
         return False
