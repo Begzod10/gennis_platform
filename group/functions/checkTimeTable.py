3a59202c7b12b8febed22f6782b839f927dfbeb4
thesobirov <shahzodsobirjonov9@gmail.com>
2024-08-16 14:45:09 +0500

    commit

--- group/functions/checkTimeTable.py
+++ group/functions/checkTimeTable.py
@@ -9,7 +9,8 @@ def check_time_table(time_tables, subject):
         'students': [],
     }
     for time_table in time_tables:
-        room = Room.objects.get(id=time_table['room'])
+        print(time_table)
+        room = Room.objects.get(id=int(time_table['room']))
         room_time_table = room.grouptimetable_set.filter(week_id=time_table['week'],
                                                          start_time__gte=time_table['start_time'],
                                                          end_time__lte=time_table['end_time']).first()
