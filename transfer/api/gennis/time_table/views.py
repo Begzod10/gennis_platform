6089bd09cf33efdba4f75dcfc16f81823dce11ea
MMR <mannopboy8@gmail.com>
2024-09-10 09:07:38 +0300

    Ignore transfer app

--- transfer/api/gennis/time_table/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import generics
-
-from time_table.models import WeekDays
-from .serializers import WeekDaysSerializerTransfer, GroupTimeTableSerializerTransfer
-
-
-class TransferWeekDaysCreate(generics.CreateAPIView):
-    queryset = WeekDays.objects.all()
-    serializer_class = WeekDaysSerializerTransfer
