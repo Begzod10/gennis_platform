a5f9573870a7deeecde071fd37dffd6c4195ee93
MMR <mannopboy8@gmail.com>
2024-09-17 13:01:15 +0300

    sdf

--- bot/views.py
+++ bot/views.py
@@ -3,7 +3,6 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from students.models import Student
 from django.db.models import Q
-from user.models import CustomUser
 from django.contrib.auth import authenticate
 from time_table.models import GroupTimeTable, WeekDays
 from attendances.models import AttendancePerMonth
