ad2bb00f969e61294b6271a82836783a6c2b9640
shahzodbeeck <shahzodomonboyev1.com>
2024-09-17 15:00:31 +0500

    fdgdf

--- attendances/urls.py
+++ attendances/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from .Api.AttendanceDatas import AttendanceDatas, AttendanceDatasForGroup, AttendanceDatasForAllGroup
 from .Api.AttendanceDelete import AttendanceDelete
 from .Api.AttendanceList import AttendanceList, AttendanceListForAllGroups
-from .Api.ToAttend import ToAttend
+from .Api.ToAttend import ToAttend, ToAttendSchool
 
 urlpatterns = [
     path('to_attend/<int:group_id>/', ToAttend.as_view(), name='to-attend'),
+    path('to_attend_school/<int:group_id>/', ToAttendSchool.as_view(), name='to-attend-school'),
     path('attendance_list/<int:group_id>/', AttendanceList.as_view(), name='attendance_list'),
     path('attendance_list_all/<int:student_id>/', AttendanceListForAllGroups.as_view(),
          name='attendance_list_all_groups'),
