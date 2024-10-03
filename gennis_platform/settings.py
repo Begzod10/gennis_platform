a9cf40a703fa22ef85140dc2430833189728b8f2
Mannopboy <147496635+Mannopboy@users.noreply.github.com>
2024-10-03 14:50:34 +0300

    Update settings.py

--- gennis_platform/settings.py
+++ gennis_platform/settings.py
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'gennis_platform.wsgi.application'
 
 # DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': 'gennis_platform', 'USER': 'postgres',
 #                          'PASSWORD': '123', 'HOST': 'localhost', 'PORT': '5432'}}
-DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': 'gennis_platform', 'USER': 'postgres',
+DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': 'gennis', 'USER': 'postgres',
                          'PASSWORD': '123', 'HOST': 'localhost', 'PORT': '5432'}}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
