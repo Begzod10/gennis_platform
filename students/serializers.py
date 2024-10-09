4241625ecdc3d25f0addbc7a6644398458fff68c
shahzodbeeck <shahzodomonboyev1.com>
2024-10-09 12:22:40 +0500

    dff

--- students/serializers.py
+++ students/serializers.py
@@ -99,9 +99,8 @@ def get_remaining_debt_for_student(student_id):
         if month.payment < 0:
             month.payment = 0
             month.save()
-        if month.remaining_debt > month.total_debt:
-            month.remaining_debt = month.total_debt - month.payment-month.discount
-            month.save()
+        month.remaining_debt = month.total_debt - month.payment-month.discount
+        month.save()
 
 
     remaining_debt_sum = AttendancePerMonth.objects.filter(
