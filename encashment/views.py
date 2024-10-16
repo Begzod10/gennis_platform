d11ae97bec86346328bec3b51b3579cc26353da4
shahzodbeeck <shahzodomonboyev1.com>
2024-10-16 23:04:34 +0500

    fgd

--- encashment/views.py
+++ encashment/views.py
@@ -322,7 +322,7 @@ class GetSchoolStudents(APIView):
                     date__year=current_year
                 ).aggregate(total=Sum('payment_sum'))['total'] or 0
 
-                total_debt += total_debt_student - discount
+                total_debt += total_debt_student
                 total_sum_test += cash_payment + bank_payment + click_payment
                 reaming_debt += remaining_debt_student
                 total_dis += discount
