97efec12f43aec1a52fbebf99be16463d1ce5d61
Begzod 10 <rimefara22@gmail.com>
2024-10-23 12:30:29 +0500

    commit

--- teachers/Api/read.py
+++ teachers/Api/read.py
@@ -47,6 +47,7 @@ class TeacherListView(QueryParamFilterMixin, generics.ListAPIView):
         return queryset
 
 
+# test
 class TeacherRetrieveView(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Teacher.objects.all()
