d8269434c3d5400d1b8b5d7232e50569f69dc555
Mannopboy <mannopboy8@gmail.com>
2024-11-05 06:22:57 +0000

    asf

--- school_time_table/Api/Hours/CreateHour.py
+++ school_time_table/Api/Hours/CreateHour.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from rest_framework import generics
 
 from rest_framework.response import Response
@@ -37,43 +36,3 @@ class HoursView(APIView):
 
         return Response(result)
 
-=======
-from rest_framework import generics
-
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-
-
-from ...models import Hours
-from ...serializers import HoursSerializers
-
-
-
-class HourListCreateView(generics.ListCreateAPIView):
-    queryset = Hours.objects.all().order_by('order')
-    serializer_class = HoursSerializers
-
-
-
-
-
-
-class HoursView(APIView):
-    def get(self, request):
-        hours_queryset = Hours.objects.all()
-
-        high_hours = hours_queryset.filter(types__name='high')
-        initial_hours = hours_queryset.filter(types__name='initial')
-
-        high_data = HoursSerializers(high_hours, many=True).data
-        initial_data = HoursSerializers(initial_hours, many=True).data
-
-        result = {
-            'high': high_data,
-            'initial': initial_data
-        }
-
-        return Response(result)
-
->>>>>>> 2ec172c7ff13fdfb0800640eb3dcd1a861fd6f29
--- school_time_table/Api/TimeTable/DeleteItemClassTimeTable.py
+++ school_time_table/Api/TimeTable/DeleteItemClassTimeTable.py
@@ -1,31 +1,9 @@
-<<<<<<< HEAD
 from rest_framework import generics
 from rest_framework.response import Response
 from ...models import ClassTimeTable
 
 from ...serializers import ClassTimeTableCreateUpdateSerializers
-from teachers.functions.school.CalculateTeacherSalary import teacher_salary_school
 
-
-class DeleteItemClassTimeTable(generics.RetrieveDestroyAPIView):
-    queryset = ClassTimeTable.objects.all()
-    serializer_class = ClassTimeTableCreateUpdateSerializers
-
-    def delete(self, request, *args, **kwargs):
-        instance = self.get_object()
-        print(instance.id)
-        teacher_salary_school(request=None, update=True, salary_id=instance.id, worked_hours=0, deleted=True)
-        self.perform_destroy(instance)
-        print(instance)
-
-        return Response({"msg": "Dars muvvaffaqqiyatli o'chirildi"})
-=======
-from rest_framework import generics
-from rest_framework.response import Response
-from ...models import ClassTimeTable
-
-from ...serializers import ClassTimeTableCreateUpdateSerializers
-from teachers.functions.school.CalculateTeacherSalary import teacher_salary_school
 
 
 class DeleteItemClassTimeTable(generics.RetrieveDestroyAPIView):
@@ -38,4 +16,3 @@ class DeleteItemClassTimeTable(generics.RetrieveDestroyAPIView):
         teacher_salary_school(request=None, update=True, salary_id=instance.id, worked_hours=0, deleted=True,
                               teacher_id=instance.teacher.id, month_date=instance.date)
         return Response({"msg": "Dars muvvaffaqqiyatli o'chirildi"})
->>>>>>> 2ec172c7ff13fdfb0800640eb3dcd1a861fd6f29
