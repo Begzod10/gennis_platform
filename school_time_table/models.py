519becf57ec436d85e87a929b064b576f43fc76b
Begzod 10 <rimefara22@gmail.com>
2024-11-07 20:59:10 +0500

    commit

--- school_time_table/models.py
+++ school_time_table/models.py
@@ -11,16 +11,7 @@ class Hours(models.Model):
     # types = models.ManyToManyField('HoursType', related_name='hours')
 
 
-# class HoursType(models.Model):
-#     name = models.CharField()
-#
 
-# @receiver(post_migrate)
-# def create_default_overhead_types(sender, **kwargs):
-#     default_values = [{'name': 'Initial'},
-#                       {'name': 'High'}]
-#     for value in default_values:
-#         HoursType.objects.get_or_create(name=value['name'])
 
 
 class ClassTimeTable(models.Model):
@@ -35,6 +26,3 @@ class ClassTimeTable(models.Model):
     name = models.CharField()
     students = models.ManyToManyField('students.Student', related_name='class_time_table')
     date = models.DateField(null=True)
-
-
-
