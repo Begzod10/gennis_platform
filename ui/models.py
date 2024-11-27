bd1f1a2886ac0e8801ae9636caeb0f31e4a04bc0
shahzodbeeck <shahzodomonboyev1.com>
2024-11-27 22:05:50 +0500

    fddg

--- ui/models.py
+++ ui/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 from django.utils import timezone
 
 
@@ -9,8 +11,17 @@ class FrontedPageType(models.Model):
         return self.name
 
 
+@receiver(post_migrate)
+def create_fronted_types(sender, **kwargs):
+    default_values = [{"name": "gallery"}, {"name": "programs"}, {"name": "student_profile"}, {"name": "news"},
+                      {"name": "vission_missoin"}, {"name": "extra_curricular"}, {"name": "curricular"},
+                      {"name": "intro"}, {"name": "certificates"}]
+    for value in default_values:
+        FrontedPageType.objects.get_or_create(**value)
+
+
 class FrontedPage(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,null=True)
     type = models.ForeignKey(FrontedPageType, on_delete=models.SET_NULL, null=True)
     description = models.TextField(null=True)
     date = models.DateField(null=True, default=timezone.now)
--- ui/views.py
+++ ui/views.py
@@ -1,4 +1,6 @@
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import FrontedPageType, FrontedPage, FrontedPageImage
 from .serializers import FrontedPageTypeSerializer, FrontedPageSerializer, FrontedPageImageSerializer
@@ -12,6 +14,18 @@ class FrontedPageTypeViewSet(viewsets.ModelViewSet):
 class FrontedPageViewSet(viewsets.ModelViewSet):
     queryset = FrontedPage.objects.all()
     serializer_class = FrontedPageSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        if kwargs['pk'] != "undefined":
+            instance = FrontedPage.objects.filter(type_id=kwargs['pk']).all()
+            serializer = self.get_serializer(instance, many=True)
+            return Response(serializer.data)
+        return Response({"msg": 'Fronteddan hatolik bo\'ldi id o\'rniga undefined keldi!!!'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        response = super().destroy(request, *args, **kwargs)
+        return Response({'message': 'deleted'}, status=status.HTTP_200_OK)
 
 
 class FrontedPageImageViewSet(viewsets.ModelViewSet):
