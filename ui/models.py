23624a2ff893b793b042678011237a605a686ffd
shahzodbeeck <shahzodomonboyev1.com>
2024-11-28 21:30:43 +0500

    asda

--- ui/models.py
+++ ui/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
-from django.utils import timezone
 
 
 class FrontedPageType(models.Model):
@@ -15,13 +14,13 @@ class FrontedPageType(models.Model):
 def create_fronted_types(sender, **kwargs):
     default_values = [{"name": "gallery"}, {"name": "programs"}, {"name": "student_profile"}, {"name": "news"},
                       {"name": "vission_missoin"}, {"name": "extra_curricular"}, {"name": "curricular"},
-                      {"name": "intro"}, {"name": "certificates"}]
+                      {"name": "intro"}, {"name": "certificates"}, {'name': 'intro_2'}]
     for value in default_values:
         FrontedPageType.objects.get_or_create(**value)
 
 
 class FrontedPage(models.Model):
-    name = models.CharField(max_length=100,null=True)
+    name = models.CharField(max_length=100, null=True)
     type = models.ForeignKey(FrontedPageType, on_delete=models.SET_NULL, null=True)
     description = models.TextField(null=True)
     date = models.DateField(null=True)
