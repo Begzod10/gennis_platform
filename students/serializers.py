cf9fd1dcca994a43ebe27f15c204f77777fef0b3
shahzodbeeck <shahzodomonboyev1.com>
2024-12-06 14:21:20 +0500

    sdsf

--- students/serializers.py
+++ students/serializers.py
@@ -101,7 +101,6 @@ def get_remaining_debt_for_student(student_id):
             month_date__month=10, month_date__year=2024
         )
     else:
-        print('chichvar')
         attendances = AttendancePerMonth.objects.filter(student_id=student_id).all()
     current_date = date.today()
     for month in attendances:
@@ -113,11 +112,20 @@ def get_remaining_debt_for_student(student_id):
             month.save()
         month.remaining_debt = month.total_debt - (month.payment + month.discount)
         month.save()
-
-    remaining_debt_sum = AttendancePerMonth.objects.filter(
-        student_id=student_id,
-        month_date__lte=current_date
-    ).aggregate(total_remaining_debt=Sum('remaining_debt'))
+    if branch_name == "Sergeli":
+        remaining_debt_sum = AttendancePerMonth.objects.filter(
+            student_id=student_id,
+            month_date__lte=current_date
+        ).exclude(
+            month_date__month=9, month_date__year=2024
+        ).exclude(
+            month_date__month=10, month_date__year=2024
+        ).aggregate(total_remaining_debt=Sum('remaining_debt'))
+    else:
+        remaining_debt_sum = AttendancePerMonth.objects.filter(
+            student_id=student_id,
+            month_date__lte=current_date
+        ).aggregate(total_remaining_debt=Sum('remaining_debt'))
     total_remaining_debt = remaining_debt_sum['total_remaining_debt'] or 0
 
     if total_remaining_debt == 0:
