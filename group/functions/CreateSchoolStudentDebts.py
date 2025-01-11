3ceacd7c285b1d1084385030286878ca91fba8a7
thesobirov <shahzodsobirjonov9@gmail.com>
2025-01-11 15:22:58 +0500

    afwecsscs

--- group/functions/CreateSchoolStudentDebts.py
+++ group/functions/CreateSchoolStudentDebts.py
@@ -36,23 +36,27 @@ def create_school_student_debts(group, students):
 #
 #     return sorted_datetime_dates
 def generate_months(start_year, start_month, end_year, end_month):
-    if start_month == 7 or start_month == 8:
-        start_month = 9
-
-    if 1 <= start_month <= 6:
-        end_month = 6
-
-    current_year = start_year
-    current_month = start_month
     dates = []
 
-    while current_year < end_year or (current_year == end_year and current_month <= end_month):
-        dates.append((current_year, current_month))
-        if current_month == 12:
-            current_month = 1
-            current_year += 1
-        else:
+    if 1 <= start_month <= 6:
+        current_year = start_year
+        current_month = start_month
+        while current_month <= 6:
+            dates.append((current_year, current_month))
             current_month += 1
+    else:
+        if start_month == 7 or start_month == 8:
+            start_month = 9
+
+        current_year = start_year
+        current_month = start_month
+        while current_year < end_year or (current_year == end_year and current_month <= 6):
+            dates.append((current_year, current_month))
+            if current_month == 12:
+                current_month = 1
+                current_year += 1
+            else:
+                current_month += 1
 
     datetime_dates = [timezone.localize(datetime(year, month, 1)) for year, month in dates]
     sorted_datetime_dates = sorted(datetime_dates)
