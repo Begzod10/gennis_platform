aa30eab1e0cfbdba34236c9b1972a9a801699d5b
shahzodbeeck <shahzodomonboyev1.com>
2025-01-14 22:04:17 +0500

    dfdsf

--- school_time_table/admin.py
+++ school_time_table/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from .models import Hours, ClassTimeTable
 
 
-# Register the Hours model
 @admin.register(Hours)
 class HoursAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_time', 'end_time', 'order')
