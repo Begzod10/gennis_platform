2cdcc7b661ecb6e9e106aea2a328205ebac51da0
shahzodbeeck <shahzodomonboyev1.com>
2025-01-14 21:23:44 +0500

    asda

--- lesson_plan/functions/utils.py
+++ lesson_plan/functions/utils.py
@@ -1,4 +1,3 @@
-
 import calendar
 from calendar import monthrange
 from datetime import datetime, date
@@ -7,7 +6,7 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 
 from lesson_plan.models import LessonPlan, Group
-from time_table.models import GroupTimeTable
+from school_time_table.models import ClassTimeTable
 
 
 def number_of_days_in_month(year, month):
@@ -19,16 +18,16 @@ def number_of_days_in_month(year, month):
 def weekday_from_date(day_list, month, year, week_list):
     filtered_days = []
     for days in day_list:
-        if calendar.day_name[
-            date(day=days, month=int(month), year=year).weekday()
-        ] in week_list:
+        weekday_name = calendar.day_name[date(year, int(month), days).weekday()]
+        if weekday_name in week_list:
             filtered_days.append(days)
     return filtered_days
 
-def update_lesson_plan( group_id):
-    time_table_group = GroupTimeTable.objects.filter(group_id=group_id).order_by('id')
 
-    week_list = [time_table.week.eng_name for time_table in time_table_group]
+def update_lesson_plan(group_id):
+    time_table_group = ClassTimeTable.objects.filter(group_id=group_id).order_by('id')
+
+    week_list = [time_table.date.date.strftime('%A') for time_table in time_table_group]
 
     current_year = datetime.now().year
     current_month = datetime.now().month
@@ -46,7 +45,7 @@ def update_lesson_plan( group_id):
             date_get = datetime.strptime(f"{current_year}-{current_month}-{day}", "%Y-%m-%d")
             exist = LessonPlan.objects.filter(date=date_get, group_id=group_id, teacher_id=group.teacher.id).exists()
             if not exist:
-                lesson_plan = LessonPlan(group_id=group_id, teacher_id=group.teacher.id, date=date_get)
+                lesson_plan = LessonPlan.objects.create(group_id=group_id, teacher_id=group.teacher.id, date=date_get)
                 lesson_plan.save()
 
     return JsonResponse({"message": "Lesson plans updated"}, status=200)
