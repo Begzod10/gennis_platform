420e82452d2590d73cdedf67466936c19d1f9b8d
shahzodbeeck <shahzodomonboyev1.com>
2025-01-14 22:10:42 +0500

    dfdsf

--- lesson_plan/functions/utils.py
+++ lesson_plan/functions/utils.py
@@ -1,6 +1,5 @@
-import calendar
-from calendar import monthrange
-from datetime import datetime, date
+from calendar import monthrange, day_name
+from datetime import datetime, timedelta, date
 
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
@@ -16,39 +15,32 @@ def number_of_days_in_month(year, month):
 
 
 def weekday_from_date(day_list, month, year, week_list):
-    filtered_days = []
-    for days in day_list:
-        if calendar.day_name[
-            date(day=days, month=int(month), year=year).weekday()
-        ] in week_list:
-            filtered_days.append(days)
-    return filtered_days
+    return [
+        days for days in day_list
+        if day_name[date(year, month, days).weekday()] in week_list
+    ]
 
 
 def update_lesson_plan(group_id):
     time_table_group = ClassTimeTable.objects.filter(group_id=group_id).order_by('id')
-
     week_list = [time_table.date.strftime('%A') for time_table in time_table_group]
 
     current_year = datetime.now().year
     current_month = datetime.now().month
-
     number_days = number_of_days_in_month(current_year, current_month)
-
-    plan_days = list(range(1, number_days + 1))
-    plan_days = weekday_from_date(plan_days, current_month, current_year, week_list)
+    plan_days = weekday_from_date(list(range(1, number_days + 1)), current_month, current_year, week_list)
 
     group = get_object_or_404(Group, id=group_id)
-    current_day2 = datetime.now().day + 5
+    current_date = datetime.now()
+    future_date_limit = current_date + timedelta(days=5)
 
     for day in plan_days:
-        if current_day2 >= day:
-            date_get = datetime.strptime(f"{current_year}-{current_month}-{day}", "%Y-%m-%d")
+        date_get = date(current_year, current_month, day)
+        if date_get <= future_date_limit.date():
             teachers = group.teacher.all()
             for teacher in teachers:
                 exist = LessonPlan.objects.filter(date=date_get, group_id=group_id, teacher_id=teacher.id).exists()
                 if not exist:
-                    lesson_plan = LessonPlan.objects.create(group_id=group_id, teacher_id=teacher.id, date=date_get)
-                    lesson_plan.save()
+                    LessonPlan.objects.create(group_id=group_id, teacher_id=teacher.id, date=date_get)
 
     return JsonResponse({"message": "Lesson plans updated"}, status=200)
