7e4953e1a86c457abf13b0b30a4d0a1201400180
shahzodbeeck <shahzodomonboyev1.com>
2025-01-28 19:24:58 +0500

    aad

--- teachers/functions/school/CalculateTeacherSalary.py
+++ teachers/functions/school/CalculateTeacherSalary.py
@@ -30,6 +30,19 @@ def calculate_teacher_salary(teacher):
                     'percentage': 50,
                 }
             )
+            month_date2 = datetime(2024, 12, 1)
+
+            TeacherSalary.objects.get_or_create(
+                teacher=teacher,
+                month_date=month_date2,
+                defaults={
+                    'total_salary': teacher.teacher_salary_type.salary,
+                    'remaining_salary': teacher.teacher_salary_type.salary,
+                    'taken_salary': 0,
+                    'total_black_salary': 0,
+                    'percentage': 50,
+                }
+            )
             # if salary.total_salary != salary.teacher.teacher_salary_type.salary:
         #     salary.total_salary = salary.teacher.teacher_salary_type.salary
         #     salary.remaining_salary = salary.teacher.teacher_salary_type.salary - salary.taken_salary
