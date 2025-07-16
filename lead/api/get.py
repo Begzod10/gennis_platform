5cfd170a0fbfddc9245a5f290966f3f7ab1ec04e
Begzod10 <rimefara22@gmail.com>
2025-07-16 18:54:32 +0500

    changed menu status

--- lead/api/get.py
+++ lead/api/get.py
@@ -184,7 +184,7 @@ class LeadListAPIView(generics.ListAPIView):
             queryset = self.get_queryset()
             if leadcall_today_ids:
                 queryset = queryset.filter(Q(pk__in=leadcall_today_ids))
-            queryset = queryset.filter(Q(pk__in=leadcall_today_ids))
+            queryset = queryset.exclude(pk__in=leadcall_today_ids)
             serializer = self.get_serializer(queryset, many=True)
             return Response({
                 "data": serializer.data,
