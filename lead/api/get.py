e869b546146c804ec7480de328335956fba827b4
Begzod10 <rimefara22@gmail.com>
2025-07-16 13:19:55 +0500

    changed menu status

--- lead/api/get.py
+++ lead/api/get.py
@@ -197,8 +197,8 @@ class LeadListAPIView(generics.ListAPIView):
         ).filter(
             Q(has_other_leadcalls=False)
         ).order_by('pk')
-        leads_by_operators = OperatorLead.objects.filter(date=selected_date, lead__in=leads).values_list('lead',
-                                                                                                         flat=True)
+        leads_by_operators = OperatorLead.objects.filter(lead__in=leads, operator=user).values_list('lead',
+                                                                                                    flat=True)
         print('leads_by_operators', len(leads_by_operators))
         # Annotate leads with call status
         # leads = leads.annotate(
