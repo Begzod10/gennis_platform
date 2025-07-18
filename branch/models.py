59cc88b810de0a8434a50923a166d444c0c7146c
shahzodbeeck <shahzodomonboyev1.com>
2025-07-18 13:11:03 +0500

    sdfs

--- branch/models.py
+++ branch/models.py
@@ -22,3 +22,6 @@ class Branch(models.Model):
     address = models.CharField(max_length=255, null=True)
     year = models.DateField(null=True)
     old_id = models.IntegerField(unique=True, null=True)
+
+    def __str__(self):
+        return self.name
