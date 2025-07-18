948c59acb95f9b9e021b899bf93be546297a61bd
shahzodbeeck <shahzodomonboyev1.com>
2025-07-18 14:24:05 +0500

    sdfs

--- user/serializers.py
+++ user/serializers.py
@@ -272,6 +272,9 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
             return self.object
 
     def validate(self, attrs):
+        from lead.models import Lead
+        Lead.objects.filter(branch__name="Gazalkent").all().update(
+            branch=Branch.objects.filter(name="Xo'jakent", branch__location__system__name='school').first())
 
         username = attrs.get('username')
         password = attrs.get('password')
