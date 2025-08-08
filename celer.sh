80c2b3b0cc37d003f99d54410b208fb699b9cff8
shahzodbeeck <shahzodomonboyev1.com>
2025-08-08 19:09:34 +0500

    sda

--- celer.sh
+++ celer.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -e
-
 source /home/ubuntu/gennis_website/venv/bin/activate
 
 echo "⏱️ Celery Beat ishga tushdi..."
@@ -8,3 +6,4 @@ celery -A config beat --loglevel=info &
 
 echo "⚙️ Celery Worker ishga tushdi..."
 celery -A config worker --loglevel=info
+wait
