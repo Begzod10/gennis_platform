fb7739832754985450083bfe5d30b143e4822ac2
Begzod10 <rimefara22@gmail.com>
2025-08-25 22:52:22 +0500

    commit

--- flows/Api/DeleteFlow.py
+++ flows/Api/DeleteFlow.py
@@ -1,9 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-
 from flows.models import Flow
-
 
 
 class DeleteFlow(APIView):
@@ -13,15 +11,23 @@ class DeleteFlow(APIView):
         except Flow.DoesNotExist:
             return Response({'error': 'Flow not found'}, status=404)
 
-        students = flow.students.all()
+        # copy related objects to avoid issues while deleting in loop
+        students = list(flow.students.all())
+        class_time_tables = list(flow.classtimetable_set.all())
+
         for student in students:
-            if flow.classtimetable_set.all():
-                for class_time_table in flow.classtimetable_set.all():
+            for class_time_table in class_time_tables:
+                if class_time_table in student.class_time_table.all():
                     student.class_time_table.remove(class_time_table)
-                    if flow.teacher is not None:
-                        flow.teacher.classtimetable_set.remove(class_time_table)
-                    class_time_table.delete()
+
+                if flow.teacher and class_time_table in flow.teacher.classtimetable_set.all():
+                    flow.teacher.classtimetable_set.remove(class_time_table)
+
+                class_time_table.delete()
+
             flow.students.remove(student)
+
+        name = flow.name
         flow.delete()
 
-        return Response({'msg': f'{flow.name} patoki ochirildi'}, status=200)
+        return Response({'msg': f'{name} patoki ochirildi'}, status=200)
