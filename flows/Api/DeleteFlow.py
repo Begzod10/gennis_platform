701e8d5832560b4e284cbf6711be54ef5adc2265
Begzod10 <rimefara22@gmail.com>
2025-08-27 13:18:51 +0500

    commit

--- flows/Api/DeleteFlow.py
+++ flows/Api/DeleteFlow.py
@@ -12,17 +12,13 @@ class DeleteFlow(APIView):
             return Response({'error': 'Flow not found'}, status=404)
 
         for class_time_table in flow.classtimetable_set.all():
-            # Avval timetable o‘chirilsin
             class_time_table.delete()
 
-        # Keyin studentlarni detach qilamiz
         flow.students.clear()
 
-        # Agar teacher bog‘langan bo‘lsa, timetable aloqalarini olib tashlash
         if flow.teacher:
             flow.teacher.classtimetable_set.clear()
 
-        # Oxirida flow o‘chirilsin
         flow.delete()
 
         return Response({'msg': f'{flow.name} patoki o‘chirildi'}, status=200)
