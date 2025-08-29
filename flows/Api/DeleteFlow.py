f6601186444c1d7b450dbcf28bedfedbb11e7929
Begzod10 <rimefara22@gmail.com>
2025-08-29 16:04:00 +0500

    commit

--- flows/Api/DeleteFlow.py
+++ flows/Api/DeleteFlow.py
@@ -5,20 +5,16 @@ from flows.models import Flow
 
 
 class DeleteFlow(APIView):
+
     def delete(self, request, pk):
         try:
             flow = Flow.objects.get(pk=pk)
         except Flow.DoesNotExist:
             return Response({'error': 'Flow not found'}, status=404)
-
         for class_time_table in flow.classtimetable_set.all():
             class_time_table.delete()
-
         flow.students.clear()
-
         if flow.teacher:
             flow.teacher.classtimetable_set.clear()
-
         flow.delete()
-
         return Response({'msg': f'{flow.name} patoki o‘chirildi'}, status=200)
