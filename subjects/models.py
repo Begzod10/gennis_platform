9b0a19f3a3f428b7f0c2cb84f9ff821486a4a015
Begzod10 <rimefara22@gmail.com>
2025-09-16 12:18:42 +0500

    commit

--- subjects/models.py
+++ subjects/models.py
@@ -9,6 +9,9 @@ class Subject(models.Model):
     classroom_id = models.IntegerField(null=True)
     disabled = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 
 class SubjectLevel(models.Model):
     name = models.CharField(max_length=250, blank=False, null=False)
