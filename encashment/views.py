7f783c607de9349492a8a2670b11ae08deec17ae
Begzod10 <rimefara22@gmail.com>
2025-10-25 16:25:19 +0500

    commit

--- encashment/views.py
+++ encashment/views.py
@@ -462,10 +462,11 @@ class GetSchoolStudents(APIView):
                 deleted_date__gte=start,  # 2025-10-01 inclusive
                 deleted_date__lt=end,  # 2025-11-01 exclusive
                 # deleted=True,  # drop this line if you don't toggle the flag
-                group__in=Subquery(active_groups_subq),  # remove if you want ANY group
+                # group__in=Subquery(active_groups_subq),  # remove if you want ANY group
             )
         )
-
+        get_deleted_data = deletions_in_period.all()
+        print(get_deleted_data)
         students_list = (
             Student.objects
             .filter(
