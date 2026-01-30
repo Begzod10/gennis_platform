efcfd116fdf8cd511cfecde5304bda8528be11c1
Begzod10 <rimefara22@gmail.com>
2026-01-30 12:01:52 +0500

    commit

--- school_time_table/Api/TimeTable/CreateClassTimeTable.py
+++ school_time_table/Api/TimeTable/CreateClassTimeTable.py
@@ -250,7 +250,7 @@ class CheckClassTimeTable(APIView):
         elif type == 'teacher':
             lesson = ClassTimeTable.objects.filter(date=date, teacher_id=checked_id, hours_id=hour).first()
 
-            lesson_for_this_room = ClassTimeTable.objects.filter(date=date, room_id=room, hours_id=hour).first()
+            lesson_for_this_room = ClassTimeTable.objects.filter(date=date, room_id=lesson.room, hours_id=hour).first()
             if lesson_for_this_room:
                 if lesson_for_this_room.room.deleted:
                     lesson_for_this_room.delete()
