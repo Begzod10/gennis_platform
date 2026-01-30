39d01a7c38cca8456de9e35d25394a81e8dbcaff
thesobirov <shahzodsobirjonov9@gmail.com>
2026-01-30 11:33:14 +0500

    acaaa

--- mobile/teachers/urls.py
+++ mobile/teachers/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     # path('attandance-students/', TeachersAttendaceStudentsListView.as_view(), name='debted-students'),
     # path('profile/', TeacherProfileView.as_view(), name='teacher-profile'),
     # path('groups/', GroupListView.as_view(), name='teacher-profile'),
-    # path('missions/', include('mobile.teachers.missions.urls'), name='missions'),
+    path('missions/', include('mobile.teachers.missions.urls'), name='missions'),
 ]
