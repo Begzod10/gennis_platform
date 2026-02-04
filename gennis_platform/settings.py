90158a27c339274860b16765c4e734dfe51b8f5f
thesobirov <shahzodsobirjonov9@gmail.com>
2026-02-04 14:48:50 +0500

    aca

--- gennis_platform/settings.py
+++ gennis_platform/settings.py
@@ -104,9 +104,12 @@ WSGI_APPLICATION = 'gennis_platform.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        # 'NAME': os.getenv('DB_NAME'),
+        'NAME': "gennis_platform",
+        # 'USER': os.getenv('DB_USER'),
+        'USER': "postgres",
+        # 'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PASSWORD': "123",
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
     }
