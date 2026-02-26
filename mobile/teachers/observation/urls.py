aba3a7d1467c77c09afc100b82166df947dc4426
Shahzod Omonboyev <shahzodomonboyev1@gmail.com>
2026-02-26 20:13:57 +0500

    observation mobile fineshed

--- mobile/teachers/observation/urls.py
+++ mobile/teachers/observation/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('observation_info_list/', ObservationInfoList.as_view(), name='observation-info-list'),
     path('observation_options/', ObservationOptionsList.as_view(), name='observation-options-list'),
     path('teacher_observe/<int:group_id>', TeacherObserveView.as_view(), name='teacher-observe'),
+    path('teacher_observe/', TeacherObserveView.as_view(), name='teacher-observe'),
     path('teacher_options/', TeacherObserveGroupView.as_view(), name='teacher-options'),
 ]
