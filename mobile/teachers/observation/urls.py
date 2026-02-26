c4adb8fab8c826461dd6c96f58251e03b42f0584
Shahzod Omonboyev <shahzodomonboyev1@gmail.com>
2026-02-26 20:17:32 +0500

    observation mobile fineshed

--- mobile/teachers/observation/urls.py
+++ mobile/teachers/observation/urls.py
@@ -5,7 +5,7 @@ from .views import ObservationInfoList, ObservationOptionsList, TeacherObserveVi
 urlpatterns = [
     path('observation_info_list/', ObservationInfoList.as_view(), name='observation-info-list'),
     path('observation_options/', ObservationOptionsList.as_view(), name='observation-options-list'),
-    path('teacher_observe/<int:group_id>', TeacherObserveView.as_view(), name='teacher-observe'),
     path('teacher_observe/', TeacherObserveView.as_view(), name='teacher-observe'),
+    path('teacher_observe/<int:group_id>', TeacherObserveView.as_view(), name='teacher-observe'),
     path('teacher_options/', TeacherObserveGroupView.as_view(), name='teacher-options'),
 ]
