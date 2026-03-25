a15e1ca02157c11caaa6271daabaece32be6cb8c
Begzod10 <rimefara22@gmail.com>
2026-03-25 12:05:42 +0500

    commit

--- tasks/Api/missions/attachment/views.py
+++ tasks/Api/missions/attachment/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
 from tasks.models import MissionAttachment
-from tasks.serializers import MissionAttachmentSerializer, _sync_attachment_to_management
+from tasks.serializers import MissionAttachmentSerializer, _sync_attachment_to_management, _sync_attachment_update_to_management, _sync_attachment_delete_to_management
 
 
 class AttachmentListCreateAPIView(generics.ListCreateAPIView):
@@ -21,7 +21,19 @@ class AttachmentDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MissionAttachment.objects.all()
     serializer_class = MissionAttachmentSerializer
 
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if instance.management_id:
+            _sync_attachment_update_to_management(
+                management_id=instance.management_id,
+                file=instance.file.name if instance.file else None,
+                note=instance.note,
+            )
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
+        mgmt_id = instance.management_id
         self.perform_destroy(instance)
+        if mgmt_id:
+            _sync_attachment_delete_to_management(mgmt_id)
         return Response({"detail": "Deleted successfully"}, status=status.HTTP_200_OK)
--- tasks/Api/missions/comment/views.py
+++ tasks/Api/missions/comment/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions
 from rest_framework import status
 from rest_framework.response import Response
 from tasks.models import MissionComment
-from tasks.serializers import MissionCommentSerializer, _sync_comment_to_management
+from tasks.serializers import MissionCommentSerializer, _sync_comment_to_management, _sync_comment_update_to_management, _sync_comment_delete_to_management
 
 
 class CommentListCreateAPIView(generics.ListCreateAPIView):
@@ -21,7 +21,19 @@ class CommentDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MissionComment.objects.all()
     serializer_class = MissionCommentSerializer
 
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if instance.management_id:
+            _sync_comment_update_to_management(
+                management_id=instance.management_id,
+                text=instance.text,
+                attachment=instance.attachment.name if instance.attachment else None,
+            )
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
+        mgmt_id = instance.management_id
         self.perform_destroy(instance)
+        if mgmt_id:
+            _sync_comment_delete_to_management(mgmt_id)
         return Response({"detail": "Deleted successfully"}, status=status.HTTP_200_OK)
