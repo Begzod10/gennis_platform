4ca053f53e6eb81e53172e674fa1a533316b8e43
Begzod10 <rimefara22@gmail.com>
2026-04-15 16:14:58 +0500

    commit

--- observation/signals.py
+++ observation/signals.py
@@ -1,14 +1,16 @@
-from django.db.models.signals import post_delete
+from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
 from observation.models import TeacherObservationDay
 
 
-@receiver(post_delete, sender=TeacherObservationDay)
+@receiver(pre_delete, sender=TeacherObservationDay)
 def revert_observation_schedule(sender, instance, **kwargs):
     """
     When a TeacherObservationDay is deleted, revert any linked
     TeacherObservationSchedule entries back to incomplete.
+    Must use pre_delete because SET_NULL runs before post_delete,
+    making the filter match nothing if post_delete is used.
     """
     from observation.models import TeacherObservationSchedule
     TeacherObservationSchedule.objects.filter(observation_day=instance).update(
