d3ede4da911bf4975a6a1e94581d33edb2208a3f
thesobirov <shahzodsobirjonov9@gmail.com>
2026-05-05 15:29:54 +0500

    asadxaaaaaa

--- teachers/Api/get.py
+++ teachers/Api/get.py
@@ -116,7 +116,7 @@ class TeacherStatAPIView(APIView):
     def get(self, request, teacher_id=None):
         date_from_str = request.query_params.get('date_from')
         date_to_str = request.query_params.get('date_to')
-        branch_id = request.query_params.get('branch_id')
+        branch_id = request.query_params.get('branch')
 
         # date_from / date_to majburiy
         if not date_from_str or not date_to_str:
